2cedb5c6c27489551bde8fbb4d66017f44de93ca
IndriesAndrei <indriesandrei@yahoo.co.uk>
2023-06-17 12:52:51 +0300

    Initial App

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Pizza from './components/Pizza'
+
+function App() {
+  return (
+    <>
+     <h1>React Pizza App</h1>
+     <Pizza />
+    </>
+  )
+}
+
+export default App
