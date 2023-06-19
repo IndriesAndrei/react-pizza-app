d70ee7fa431d215055868aa88970b358006c229a
IndriesAndrei <indriesandrei@yahoo.co.uk>
2023-06-19 12:52:53 +0300

    Conditional rendering of Menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -46,14 +46,21 @@ const pizzaData = [
   ];
 
 function Menu() {
+    const pizzas = pizzaData;
+    const numPizzas = pizzas.length;
+
     return (
         <main className="menu">
             <h2>Our Menu</h2>
-            <ul className='pizzas'>
-                {pizzaData.map((pizza) => 
+            {numPizzas > 0 ? (
+              <ul className='pizzas'>
+                {pizzas.map((pizza) => 
                   <Pizza pizzaObj={pizza} key={pizza.name} />
                 )}
-            </ul>
+              </ul>
+            ) : (
+              <p>We're still working on our menu. Please come back later!</p>
+            )}
         </main>
     )
 }
