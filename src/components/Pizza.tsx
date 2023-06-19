10b68fa3d7e1963867adc233c2f814efd7c9522a
IndriesAndrei <indriesandrei@yahoo.co.uk>
2023-06-19 14:48:17 +0300

    Conditionaly show price

--- src/components/Pizza.tsx
+++ src/components/Pizza.tsx
@@ -1,11 +1,11 @@
-function Pizza({pizzaObj}: {pizzaObj: {name: string, photoName: string, ingredients: string, price: number }}) {
+function Pizza({pizzaObj}: {pizzaObj: {name: string, photoName: string, ingredients: string, price: number, soldOut: string }}) {
     return (
-        <li className="pizza">
+        <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ''}`}>
           <img src={pizzaObj.photoName} alt={pizzaObj.name} />
           <div>
             <h3>{pizzaObj.name}</h3>
             <p>{pizzaObj.ingredients}</p>
-            <span>Price: ${pizzaObj.price}</span>
+            <span>{`${pizzaObj.soldOut ? "SOLD OUT" : '$' + pizzaObj.price }`}</span>
           </div>
         </li>
     );
