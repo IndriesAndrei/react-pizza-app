111a34d0b013efc230505c65a70ebf134bc22bfe
IndriesAndrei <indriesandrei@yahoo.co.uk>
2023-06-19 12:03:59 +0300

    Footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,12 @@ function Footer() {
 
     return (
         <footer>
-            <p>{new Date().toLocaleTimeString()} - We're currently open!</p>
+            {isOpen && (
+                <div className="order">
+                    <p>We're open until {closeHour}:00. Come visit us or order online!</p>
+                    <button className="btn">Order</button>
+                </div>
+            )}
         </footer>
     )
 }
