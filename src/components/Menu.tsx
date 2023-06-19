155a470c5090cd04d4e780bbda8a63b1d50995c9
IndriesAndrei <indriesandrei@yahoo.co.uk>
2023-06-19 12:04:04 +0300

    Footer styling

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -51,7 +51,7 @@ function Menu() {
             <h2>Our Menu</h2>
             <ul className='pizzas'>
                 {pizzaData.map((pizza) => 
-                    <Pizza pizzaObj={pizza} key={pizza.name} />
+                  <Pizza pizzaObj={pizza} key={pizza.name} />
                 )}
             </ul>
         </main>
